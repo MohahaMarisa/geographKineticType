1295fb46d857d573ca794b07dc96b9b45a395902
Marisa <contactmarisalu@gmail.com>
2019-11-14 22:12:39 -0800

    tweaked a bunch of numbers, and the sampling around the letters are denser

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var timeKeeper = 0;
 var wanderers = [];
 let noiseScale = 1000;//bigger the scale the larger the over all curves 
-let noiseStrength = 20; //bigger the strength the more divergent the lines are from each other
+let noiseStrength = 18; //bigger the strength the more divergent the lines are from each other
 
 let klimFont;
 
@@ -12,7 +12,6 @@ let img;
 
 var crawlx = 0;
 var crawly = 0;
-// var wantTransparent = true;
 function preload(){
   klimFont  = loadFont('Geograph-Bold.otf');
   img = loadImage('geograph.png');
@@ -27,13 +26,13 @@ function setup() {
     var wantTransparent = true;
     // image(img, 0,0, width, height);
     textFont(klimFont);
-    textSize(350);
+    textSize(280);
     fill(0,20);
     stroke(255);
     textAlign(CENTER, CENTER);
     text("GEO", width/2, height/3);
-    text("GRAPH", width/2, 2*height/3);
-    for(let y = height/5; y <height; y+=10){
+    text("GRAPH", width/2, 2 * height/3);
+    for(let y = height/5; y < height; y+=10){
       for(let x = 0; x < width; x +=1){
         var rgba = get(x, y);
         console.log("rgba = "+ rgba);
@@ -44,11 +43,18 @@ function setup() {
         }
       }
     }
+    for(let x = width/5; x < width; x+=10){
+      for(let y = height/5; y < height*0.9; y +=1){
+        var rgba = get(x, y);
+        console.log("rgba = "+ rgba);
+        if(rgba[0] > 0){
+          let something = new Wanderer(x, y);
+          wanderers.push(something);
+          console.log("made a particle");
+        }
+      }
+    }
   }
-  // for(let i = 0; i < 400; i +=10){
-  //   let something = new Wanderer(i,0);
-  //   wanderers.push(something);
-  // }
   background(0);
 }
 
@@ -77,21 +83,21 @@ function draw() {
   //     }
   //   }
   // }
-  if (timeKeeper < 300){
-  timeKeeper +=1;
-  background(0,20);
-  // textFont(klimFont);
-  // textSize(350);
-  // fill(0,20);
-  // textAlign(CENTER, CENTER);
-  // text("GEO", width/2, height/3);
-  // text("GRAPH", width/2, 2*height/3);
-  //noiseStrength = 10+20*noise(timeKeeper/100);
-  for(let i = 0; i < wanderers.length; i++){
-    wanderers[i].move();
-    wanderers[i].display();
+  if (timeKeeper < 600){
+    timeKeeper +=1;
+    // background(0,1);
+    textFont(klimFont);
+    textSize(280);
+    fill(0,1);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    text("GEO", width/2, height/3);
+    text("GRAPH", width/2, 2 * height/3);
+    for(let i = 0; i < wanderers.length; i++){
+      wanderers[i].move();
+      wanderers[i].display();
+    }
   }
-}
 }
 function mouseMoved(){
   if(canGenerate){
@@ -104,7 +110,7 @@ function mouseMoved(){
   
 }
 function keyPressed(){
-
+  canGenerate = !canGenerate;
 }
 
 class Wanderer{
@@ -116,26 +122,30 @@ class Wanderer{
     this.currentX = this.originX;
     this.currentY = this.originY;
     this.angle = 0;
+    this.deltaAngle = 0;
     
-    this.stepSize = 1;
+    this.stepSize = 4;
   }
   move(){
-    this.angle = noise(this.currentX / noiseScale, this.currentY / noiseScale)* noiseStrength;
+    var newAngle = noise(this.currentX / noiseScale, this.currentY / noiseScale)* noiseStrength;
+    // this.deltaAngle = this.angle - newAngle;
+    this.angle = newAngle;
     this.prevX = this.currentX;
     this.prevY = this.currentY;
     
     this.currentX += cos(this.angle) * this.stepSize;
     this.currentY += sin(this.angle) * this.stepSize;
     
-    this.stepSize = log(timeKeeper);
+    // this.stepSize = log(timeKeeper);
     
   }
   display(){
     push();
     // let sColor = map(this.angle, 0, 3, 255, 0);
     // let sColor = constrain(255-(timeKeeper*timeKeeper)/10, 20, 255);
-    let sColor = (1000/(timeKeeper+1)) * 20;
-    stroke(sColor, sColor);
+    // let sColor = (1000/(timeKeeper+1)) * 2 + map(abs(this.deltaAngle), 0, 0.2, 5,200);
+    let sColor = (1000/(timeKeeper+1)) * 2;
+    stroke(255, sColor);
     strokeWeight(1);
     line(this.prevX, this.prevY, this.currentX, this.currentY);
     pop();
